cb12b44646d7d67b895d820f35c57f83716c1f62
Hardik Desai <hp676913@gmail.com>
2022-07-31 17:11:05 +0530

    Improving dark mode in about also fix some buggs

--- src/components/About.js
+++ src/components/About.js
@@ -1,72 +1,54 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-export default function About() {
-    const [text, changeText] = useState("Toogle Dark mode")
-    const [style, setStyle] = useState({
-        color: "black",
-        backgroundColor: "white"
-    });
-
-    const toogleMode = () => {
-        if (style.color === "black") {
-            setStyle({
-                color: "white",
-                backgroundColor: "black"
-            })
-            changeText("Toogle Light mode")
-        } else {
-            setStyle({
-                color: "black",
-                backgroundColor: "white"
-            })
-            changeText("Toogle Dark mode");
-        }
+export default function About(parms) {
+    // const [style, setStyle] = useState({
+    //     color: "black",
+    //     backgroundColor: "white"
+    // });
+    let myStyle = {
+        color: parms.mode==='dark' ? 'white' : 'black',
+        backgroundColor: parms.mode==='dark' ? 'rgb(3 39 67)' : 'white'
     }
+    
     return (
         <>
-            <h1 style={style}>About</h1>
+            <h1 style={myStyle}>About</h1>
             <div className="accordion" id="accordionPanelsStayOpenExample">
-                <div className="accordion-item" style={style} >
+                <div className="accordion-item" style={myStyle} >
                     <h2 className="accordion-header" id="panelsStayOpen-headingOne">
-                        <button style={style} className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                            Accordion Item #1
+                        <button style={myStyle} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                            <strong>Analyze Your text </strong>
                         </button>
                     </h2>
                     <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
                         <div className="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad non velit provident maxime maiores, corporis dolores enim officiis earum. Suscipit.
-                        </div>
+                        Free software utility which allows you to find the most frequent phrases and frequencies of words. Non-English language texts are supported. It also counts number of words, characters, sentences and syllables. Also calculates lexical density.
+                         </div>
                     </div>
                 </div>
-                <div className="accordion-item" style={style}>
+                <div className="accordion-item" style={myStyle}>
                     <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
-                        <button style={style} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                            Accordion Item #2
+                        <button style={myStyle} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                            <strong>Free to use</strong>
                         </button>
                     </h2>
                     <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
                         <div className="accordion-body">
-                            <strong>This is the second item's accordion body.</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi possimus ullam dolore eveniet dicta! Quae possimus voluptates explicabo ea unde.
+                        This web app is free to use , you can use it for free. also you can use all the code for your own project. all features are available for free.
                         </div>
                     </div>
                 </div>
-                <div className="accordion-item" style={style}>
+                <div className="accordion-item" style={myStyle}>
                     <h2 className="accordion-header" id="panelsStayOpen-headingThree">
-                        <button style={style} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                            Accordion Item #3
+                        <button style={myStyle} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                           <strong>Browser Compatible</strong>
                         </button>
                     </h2>
                     <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
                         <div className="accordion-body">
-                            <strong>This is the third item's accordion body.</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate obcaecati voluptas quo incidunt inventore deserunt repellendus, dolorum assumenda pariatur cumque?
+                           This website are browser compatible. all browser like chrome, firefox, opera, safari, edge, ie11, ie10, ie9, ie8, ie7, ie6, ie5, ie4, ie3, ie2, ie1 are supported.
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="container">
-                <div className="form-check form-switch my-2">
-                    <input onChange={toogleMode} className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{text}</label>
                 </div>
             </div>
         </>
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,8 +6,12 @@ function Alert(parms) {
         return newWord.charAt(0).toUpperCase() + newWord.slice('1');
     }
     return (
-        parms.alert && <div class={`alert alert-${parms.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{toCapitalize(parms.alert.type)}</strong>: {parms.alert.msg}
+        <div style={{ height: '45px' }}>
+            {
+                parms.alert && <div className={`alert alert-${parms.alert.type} alert-dismissible fade show`} role="alert">
+                    <strong>{toCapitalize(parms.alert.type)}</strong>: {parms.alert.msg}
+                </div>
+            }
         </div>
     )
 }
